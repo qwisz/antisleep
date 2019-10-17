52b4892793fb2a5ed1ead5189722d5aa45036a48
qwisz <barybin.anton@gmail.com>
2019-10-17 13:13:10 +0300

    add train functions and utils

---
+++ trainee.py
@@ -0,0 +1,45 @@
+import torch
+from utils import warmup_lr_scheduler
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+
+    lr_scheduler = None
+    if epoch == 0:
+        warmup_factor = 1. / 1000
+        warmup_iters = min(1000, len(data_loader) - 1)
+
+        lr_scheduler = warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
+
+    loss_accum = 0
+    for i_step, (images, targets) in enumerate(data_loader):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+        loss_accum += float(losses)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+
+    ave_loss = loss_accum / i_step
+
+    return ave_loss
+
+def train_model(model, optimizer, lr_scheduler, data_loader, device, num_epochs):
+    loss_history = []
+
+    for epoch in range(num_epochs):
+        loss = train_one_epoch(model, optimizer, data_loader, device, epoch)
+        loss_history.append(loss)
+        lr_scheduler.step()
+
+    return loss_history
---
+++ utils.py
@@ -0,0 +1,58 @@
+import torch
+from torchvision.transforms import functional as F
+import random
+
+
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        return image, target
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, prob):
+        self.prob = prob
+
+    def __call__(self, image, target):
+        if random.random() < self.prob:
+            height, width = image.shape[-2:]
+            image = image.flip(-1)
+            bbox = target['boxes']
+            bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
+            target['boxes'] = bbox
+        return image, target
+
+
+def get_transform(train):
+    transforms = []
+    # converts the image, a PIL image, into a PyTorch Tensor
+    transforms.append(ToTensor())
+    if train:
+        # during training, randomly flip the training images
+        # and ground-truth for data augmentation
+        transforms.append(RandomHorizontalFlip(0.5))
+    return Compose(transforms)
+
+
+def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor):
+    def f(x):
+        if x >= warmup_iters:
+            return 1
+        alpha = float(x) / warmup_iters
+        return warmup_factor * (1 - alpha) + alpha
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, f)
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
