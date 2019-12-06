2876d1130fd995e2af28acd5bea212b833387530
qwisz <barybin.anton@gmail.com>
2019-12-06 18:10:29 +0300

    ...

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from eyes_classifier.main import main
 face_detector_path = 'model_weights/face_detector_opencv.pt'
 eyes_classifier_path = 'model_weights/eyes_mobilenet_opencv.pt'
 video_path = '123.avi'
+output_path = 'output.avi'
 
-main(face_detector_path, eyes_classifier_path, video_path)
+main(face_detector_path, eyes_classifier_path, video_path, output_path)
